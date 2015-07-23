b4461a143c2356d471e15ff51839dba95f0ef237
twb0223 <twb-008@163.com>
2015-07-23 18:17:18 +0800

    add product img

---
+++ O2OApi/Base.Model/BreakRecord.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaseData.Model
+{
+    /// <summary>
+    /// 货损记录
+    /// </summary>
+    public class BreakRecord
+    {
+        /// <summary>
+        /// 记录ID
+        /// </summary>
+        [Key]
+        public string BreakRecordID { get; set; }
+
+        [MaxLength(100)]
+        public string ProductCode { get; set; }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        [Required]
+        public int Num { get; set; }
+
+        /// <summary>
+        /// 经办人
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string CreateBy { get; set; }
+
+        /// <summary>
+        /// 经办时间
+        /// </summary>
+        [Required]
+        public DateTime CreateAt { get; set; }
+    }
+}
---
+++ O2OApi/BaseData.Web/Controllers/UploadApiController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+using BaseData.DataAccess;
+using BaseData.Model;
+using System.Data.Entity;
+
+
+namespace BaseData.Web.Controllers
+{
+    public class UploadApiController : ApiController
+    {
+        private MyDataContext db = new MyDataContext();
+        public async Task<HttpResponseMessage> PostFormData()
+        {
+            HttpContextBase context = (HttpContextBase)Request.Properties["MS_HttpContext"];//获取传统context     
+            HttpRequestBase request = context.Request;//定义传统request对象
+
+            string pcode = request.Form["code"];//获取传递端产品条码
+
+
+
+            // Check if the request contains multipart/form-data.
+            // 检查该请求是否含有multipart/form-data
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            string root = HttpContext.Current.Server.MapPath("~/UploadFiles");
+            // var provider = new MultipartFormDataStreamProvider(root);
+            var provider = new RenamingMultipartFormDataStreamProvider(root);
+            try
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+                string imgurl = "";
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    imgurl = file.LocalFileName.Replace(root + "\\", "");
+                }
+                //更新产品对应端图片，以;分割
+                var model = await db.Products.FindAsync(pcode);
+                model.ImgUrl += imgurl+";";
+                db.Entry(model).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+
+        }
+    }
+    public class RenamingMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
+    {
+        public string Root { get; set; }
+        public RenamingMultipartFormDataStreamProvider(string root)
+            : base(root)
+        {
+            Root = root;
+        }
+        public override string GetLocalFileName(HttpContentHeaders headers)
+        {
+            string filePath = headers.ContentDisposition.FileName;
+            // Multipart requests with the file name seem to always include quotes.
+            if (filePath.StartsWith(@"""") && filePath.EndsWith(@""""))
+                filePath = filePath.Substring(1, filePath.Length - 2);
+
+            var filename = Guid.NewGuid().ToString();
+            var extension = Path.GetExtension(filePath);
+            var contentType = headers.ContentType.MediaType;
+            return filename+extension;
+        }
+
+    }
+}
